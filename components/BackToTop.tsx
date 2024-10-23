2cc77806bf6c323f31ef62d4a76ed22995d01ee0
joshuashalla1 <joshuashalla1@gmail.com>
2024-10-23 10:00:27 +0000

    made some fixes to Backttotop

--- components/BackToTop.tsx
+++ components/BackToTop.tsx
@@ -22,8 +22,8 @@ const BackToTop = () => {
   };
 
   return (
-    <div className={`back-to-top border-red-600 h-4 w-4 fixed bottom-10 right-10 bg-blue-600 rounded-full ${showButton ? 'show' : ''}`} onClick={scrollToTop}>
-      <svg xmlns="http://www.w3.org/2000/svg" className='fill-current text-white' viewBox="0 0 384 512"><path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2 160 448c0 17.7 14.3 32 32 32s32-14.3 32-32l0-306.7L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg>
+    <div className={`back-to-top border-red-600 p-2 flex flex-row justify-center place-items-center fixed bottom-10 right-10 bg-blue-600 rounded-full ${showButton ? 'show' : ''}`} onClick={scrollToTop}>
+      <svg xmlns="http://www.w3.org/2000/svg" className='h-4 w-4 fill-current text-white' viewBox="0 0 384 512"><path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2 160 448c0 17.7 14.3 32 32 32s32-14.3 32-32l0-306.7L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg>
     </div>
   );
 };
